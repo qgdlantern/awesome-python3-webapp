dedbdcd1b8e900c0977b461beb4c3e6365c62463
qgdlantern <qgdlantern@gmail.com>
2021-11-04 16:24:33 +0800

    修改

--- www/test.py
+++ www/test.py
@@ -1,12 +1,13 @@
 import orm
 import asyncio
+import hashlib
 from models import User, Blog, Comment
 
 async def test(loop):                      
     # *** 注意此处的密码填自己设的密码 ***
     await orm.create_pool(loop=loop, user='qgdlantern', password='zyt20041129', db='awesome')
     # *** 注意此处的密码填自己设的密码 ***
-    u = User(name='qgdlantrn', email='test@qq.com', passwd='1234567890', image='about:blank')
+    u = User(name='qgdlantrn', email='freehappy@gmail.com', passwd=hashlib.md5('12345678'.encode('utf-8')).hexdigest(), image='about:blank')
     await u.save()
     ## 网友指出添加到数据库后需要关闭连接池，否则会报错 RuntimeError: Event loop is closed
     orm.__pool.close()
